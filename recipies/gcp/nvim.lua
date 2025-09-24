458e08ea3c6f2b8c59ac76de6b89a4164d112aa4
Marco Kellershoff <marco@mistweaverco.com>
2025-09-24 16:08:16 +0200

    feat(gcp): add neovim 0.11.3 with kulala.nvim http client

---
+++ recipies/gcp/nvim.lua
@@ -0,0 +1,27 @@
+-- Bootstrapping lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Install Plugins
+require("lazy").setup({
+  {
+    "mistweaverco/kulala.nvim",
+    opts = {},
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+    end,
+  },
+})
