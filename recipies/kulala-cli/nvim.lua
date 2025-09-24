910765f32388264f757eab6329a1597f72175f89
Marco Kellershoff <marco@mistweaverco.com>
2025-09-24 21:51:50 +0200

    feat(kulala-cli): add kulala-cli image

---
+++ recipies/kulala-cli/nvim.lua
@@ -0,0 +1,28 @@
+-- Bootstrapping lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Install Plugins
+require("lazy").setup({
+  {
+    "mistweaverco/kulala.nvim",
+    version = "v5.3.0",
+    opts = {},
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+      pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+    end,
+  },
+})
